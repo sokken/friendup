8d618e85f2a3ca3f6eece7404b2a1c1e8291c189
Espen Olsen <goodevening.mr.olsen@gmail.com>
2026-03-10 14:58:50 +0000

    asd

--- interfaces/web_desktop/js/workspace.js
+++ interfaces/web_desktop/js/workspace.js
@@ -645,10 +645,6 @@ Workspace = {
 		
 		friendApp.showPunchClock = function() {
 			const self = this
-			console.log( 'restore_session' )
-			friendApp.restore_session()
-			return
-			
 			console.log( 'showPunchClock' )
 			window.push_log( 'showPunchClock' )
 			
